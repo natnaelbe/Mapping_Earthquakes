430d09f8a3a03a151ef1222d9d9429af5856984e
Natnael <berhanunatnael@gmail.com>
2022-09-17 12:45:00 -0700

    initial commit

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -17,6 +17,22 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
         maxZoom: 18,
         accessToken: API_KEY
     });
+// Loop through the cities array and create one marker for each city.
+cities.forEach(function(city) {
+    console.log(city)
+    L.marker(city.location).addTo(map);
+});
+// Get data from cities.js
+let cityData = cities;
+ // Loop through the cities array and create one marker for each city.
+ cityData.forEach(function(city) {
+    console.log(city)
+    L.marker(city.location, {
+        radius: city.population/100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
 
 
 
